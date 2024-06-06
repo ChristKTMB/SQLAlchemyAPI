aab82107ed98186edd12e1097932edab02a9b53e
Christ KATUMBA <christkatumba02@gmail.com>
2024-06-06 14:10:13 +0100

    Modification de faker data

--- fakerData.py
+++ fakerData.py
@@ -1,8 +1,7 @@
 from sqlalchemy.orm import declarative_base
 from sqlalchemy import Column, Integer, String
-from random import choice
+from faker import Faker
 from main import engine, session
-import string
 
 # Créer une instance de Base
 Base = declarative_base()
@@ -16,20 +15,22 @@ class Users(Base):
     last_name = Column(String(255)) 
     first_name = Column(String(255))
     email = Column(String(255))
-    phone = Column(String(20))
+    phone = Column(String(50))
     password = Column(String(255)) 
 
 # Créer les tables dans la base de données
 Base.metadata.create_all(engine)
 
-# Générez 1500 utilisateurs aléatoires
-for _ in range(20):
-    username = ''.join(choice(string.ascii_letters) for _ in range(10))
-    last_name = ''.join(choice(string.ascii_letters) for _ in range(10))
-    first_name = ''.join(choice(string.ascii_letters) for _ in range(10))
-    email = ''.join(choice(string.ascii_letters) for _ in range(10)) + '@example.com'
-    phone = ''.join(choice(string.digits) for _ in range(10))
-    password = ''.join(choice(string.ascii_letters + string.digits) for _ in range(10))
+# Générez 1500 utilisateurs aléatoires avec Faker
+fake = Faker()
+
+for _ in range(100):
+    username = fake.user_name()
+    last_name = fake.last_name()
+    first_name = fake.first_name()
+    email = fake.email()
+    phone = fake.phone_number()
+    password = fake.password()
 
     user = Users(username=username, last_name=last_name, first_name=first_name, email=email, phone=phone, password=password)
     session.add(user)
